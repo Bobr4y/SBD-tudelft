4206e16bcfb09fdc524072357cc3e516458d22c1
Bob <bobluppes@live.nl>
2019-09-19 13:24:42 +0200

    RDD Implementation

--- lab1/src/main/scala/Test.scala
+++ lab1/src/main/scala/Test.scala
@@ -1,19 +1,7 @@
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.types._
-import org.apache.spark.sql._
-
-import java.sql.Timestamp
-import java.sql.Date
-
 import org.apache.log4j.{Level, Logger}
 
 object Test {
-
-  case class DateResult (
-    Date: String,
-    AllNames: String
-  )
-
   def main(args: Array[String]) {
     Logger.getLogger("org.apache.spark").setLevel(Level.ERROR)
 
@@ -22,71 +10,23 @@ object Test {
 
     import spark.implicits._
 
-    val schema = StructType(
-      Array(
-        StructField("GKGRECORDID", StringType),
-        StructField("DATE", StringType),
-        StructField("SourceCollectionIdentifier", StringType),
-        StructField("SourceCommonName", StringType),
-        StructField("DocumentIdentifier", StringType),
-        StructField("Counts", StringType),
-        StructField("V2Counts", StringType),
-        StructField("Themes", StringType),
-        StructField("V2Themes", StringType),
-        StructField("Locations",StringType),
-        StructField("V2Locations",StringType),
-        StructField("Persons",StringType),
-        StructField("V2Persons",StringType),
-        StructField("Organizations",StringType),
-        StructField("V2Organizations",StringType),
-        StructField("V2Tone", StringType),
-        StructField("Dates",StringType),
-        StructField("GCAM", StringType),
-        StructField("SharingImage", StringType),
-        StructField("RelatedImages",StringType),
-        StructField("SocialImageEmbeds",StringType),
-        StructField("SocialVideoEmbeds",StringType),
-        StructField("Quotations", StringType),
-        StructField("AllNames", StringType),
-        StructField("Amounts",StringType),
-        StructField("TranslationInfo",StringType),
-        StructField("Extras", StringType)
-      )
-    )
+    // val df = spark.read
+    //   .format("csv")
+    //   .option("delimiter", "\t")
+    //   .schema(schema)
+    //   .load("./data/segment/20150218230000.gkg.csv")
 
-    val df = spark.read
-      .format("csv")
-      .option("delimiter", "\t")
-      .schema(schema)
-      .load("./data/segment/20150218230000.gkg.csv")
+    val rdd = sc.textFile("./data/segment/*.csv")
 
-    df.createOrReplaceTempView("gdelt")
+    val allNames = rdd.filter(line => line.split("\t").length > 23)map(_.split("\t")(23))
+    val names = allNames.flatMap(line => line.split(";"))
+    val topics = names.filter(str => str.split(",").length > 1).map({case(str) => (str.split(",")(0), str.split(",")(1).toInt)})
 
-    val dr = spark.sql("SELECT DATE,AllNames FROM gdelt WHERE DATE=\"20150218230000\"").as[DateResult]
+    val reducedTopics = topics.reduceByKey(_ + _)
 
-    //allNames.take(10).foreach(println)
-    //topics.take(3).foreach(a => println(a.AllNames))
+    val sortedTopics = reducedTopics.sortBy(_._2, false)
 
-    val topicResults = dr.flatMap(x => {
-      x.AllNames.split(";")
-    })
-
-    val result = topicResults.flatMap(x => {
-      val topic = x.split(",")(0)
-      val count = x.split(",")(1)
-      topic.map( a => (a, count))
-    })
-
-    result.take(10).foreach(println)
-
-    // val singleTopics = topics.flatMap( arr => {
-    //   val date = arr.Date
-    //   val allTopics = arr.AllNames
-    //   val topic = allTopics.split(";")
-    //   topic.map( a => (date, a))
-    // })
-
-    // singleTopics.take(10).foreach(println)
+    sortedTopics.take(30).foreach(println)
 
     spark.stop()
   }
