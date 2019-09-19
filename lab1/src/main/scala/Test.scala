e2f47cddd17fd232506e7d9b39e03c4a0a13c95c
Bob <bobluppes@live.nl>
2019-09-19 14:44:10 +0200

    Implemented excludes list

--- lab1/src/main/scala/Test.scala
+++ lab1/src/main/scala/Test.scala
@@ -3,30 +3,46 @@ import org.apache.log4j.{Level, Logger}
 
 object Test {
   def main(args: Array[String]) {
+    // Set log level to error to avoid all info logs
     Logger.getLogger("org.apache.spark").setLevel(Level.ERROR)
 
+    // Start spark session
     val spark = SparkSession.builder.master("local").appName("Test").getOrCreate()
     val sc = spark.sparkContext
-
     import spark.implicits._
 
-    // val df = spark.read
-    //   .format("csv")
-    //   .option("delimiter", "\t")
-    //   .schema(schema)
-    //   .load("./data/segment/20150218230000.gkg.csv")
+    // Define topics to exclude from the results
+    val exclude = List(
+      "Type ParentCategory",
+      "CategoryType ParentCategory",
+      "Comment Tag",
+      "View Variables Category Comment"
+    )
 
+    // Load all GDELT .csv files into an rdd
     val rdd = sc.textFile("./data/segment/*.csv")
 
-    val allNames = rdd.filter(line => line.split("\t").length > 23)map(_.split("\t")(23))
-    val names = allNames.flatMap(line => line.split(";"))
-    val topics = names.filter(str => str.split(",").length > 1).map({case(str) => (str.split(",")(0), str.split(",")(1).toInt)})
+    // AllNames contains all topics and their counts
+    val allNames = rdd
+      .filter(line => line.split("\t").length > 23) // Only consider lines that have an AllNames column (index 23)
+      .map(_.split("\t")(23)) // Retrieve the AllNames column
 
+    // Names countains a list of strings with a single topic and its count in the string
+    val names = allNames.flatMap(line => line.split(";")) // Separate topics from allNames to get a single topic and its count
+
+    // Topics splits the string into a topic name part and a count part
+    val topics = names
+      .filter(str => str.split(",").length > 1) // Only consider strings that contain a topic and a count
+      .filter(str => !(exclude.contains(str.split(",")(0))) )
+      .map({case(str) => (str.split(",")(0), str.split(",")(1).toInt)}) // Put the topic and the count into a map
+
+    // Reduce the topics by key (topics) and add the values (counts)
     val reducedTopics = topics.reduceByKey(_ + _)
 
+    // Sort by descending values (counts)
     val sortedTopics = reducedTopics.sortBy(_._2, false)
 
-    sortedTopics.take(30).foreach(println)
+    sortedTopics.take(10).foreach(println)
 
     spark.stop()
   }
